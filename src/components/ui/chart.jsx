0421ef80eece28fb7a59668348e7433a4a901650
seannywoot <202310319@gordoncollege.edu.ph>
2025-11-25 22:57:38 +0800

    Added better spacing in transaction trend tooltip

--- src/components/ui/chart.jsx
+++ src/components/ui/chart.jsx
@@ -193,7 +193,7 @@ const ChartTooltipContent = React.forwardRef(
                                         )}
                                         <div
                                             className={cn(
-                                                "flex flex-1 justify-between leading-none",
+                                                "flex flex-1 justify-between gap-6 leading-none",
                                                 nestLabel ? "items-end" : "items-center"
                                             )}
                                         >
